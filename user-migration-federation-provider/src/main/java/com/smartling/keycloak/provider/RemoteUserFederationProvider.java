95718ebdcf6dd029556cb76d55ebc3eedc1cb582
Richard <richie765@gmail.com>
2017-10-15 19:34:43 +0200

    Cleanup modules

--- user-migration-federation-provider/src/main/java/com/smartling/keycloak/provider/RemoteUserFederationProvider.java
+++ user-migration-federation-provider/src/main/java/com/smartling/keycloak/provider/RemoteUserFederationProvider.java
@@ -23,31 +23,28 @@ import org.jboss.logging.Logger;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-import org.keycloak.models.CredentialValidationOutput;
 import org.keycloak.component.ComponentModel;
-import org.keycloak.credential.CredentialModel;
 import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.CredentialInputUpdater;
+import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.credential.CredentialModel;
 import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserCredentialModel;
-import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.models.UserModel;
 import org.keycloak.storage.ReadOnlyException;
 import org.keycloak.storage.StorageId;
+import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.user.UserLookupProvider;
-import org.keycloak.credential.CredentialInputValidator;
-import org.keycloak.credential.CredentialInputUpdater;
-import org.keycloak.storage.user.UserRegistrationProvider;
 
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.core.Response;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
 
 /**
  * Remote API based user federation provider.
